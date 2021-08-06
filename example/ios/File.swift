bc2288db6d2aa44401d16fdd2822b413be59319f
Ravi K Maurya <ravi.maurya@nickelfox.com>
2021-08-06 19:29:10 +0530

    remove example

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  SharinganRnModalDropdownExample
-//
-
-import Foundation
--- example/src/validation.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup';
-export const DropdownValidation = Yup.object().shape({
-  msChipFlat: Yup.array().required(),
-  msChipOutlined: Yup.array().required(),
-  sddWoAvatar: Yup.string().required(),
-  sddWAvatar: Yup.string().required(),
-  gddWoAvatar: Yup.string().required(),
-  gddWAvatar: Yup.string().required(),
-});
